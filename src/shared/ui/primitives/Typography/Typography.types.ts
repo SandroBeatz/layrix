c31428803b68b544dcb67a267df97e02919a5426
SandroBeatz <sandro23.12.30beatz@gmail.com>
2026-01-18 15:21:45 +0600

    Add Typography component with styles and types; implement public API

---
+++ src/shared/ui/primitives/Typography/Typography.types.ts
@@ -0,0 +1,34 @@
+import type { NamedColor } from 'quasar';
+
+/**
+ * Typography component props
+ */
+export interface TypographyProps {
+  as?: keyof HTMLElementTagNameMap;
+  variant?: TypographyVariant | undefined;
+  content?: string | undefined;
+  weight?: 'light' | 'regular' | 'medium' | 'semibold' | 'bold' | undefined;
+  align?: 'left' | 'center' | 'right' | 'justify' | undefined;
+  color?: NamedColor | undefined;
+  className?: string | undefined;
+}
+
+/**
+ * Typography variants
+ */
+type TypographyVariant =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'subtitle'
+  | 'subtitle-muted'
+  | 'body'
+  | 'body-muted'
+  | 'caption'
+  | 'caption-muted'
+  | 'overline'
+  | 'overline-muted'
+  | 'blockquote';
---
+++ src/shared/ui/primitives/Typography/Typography.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import type { TypographyProps } from './Typography.types';
+import { computed } from 'vue';
+
+const props = withDefaults(defineProps<TypographyProps>(), {
+  as: 'p',
+  variant: 'body',
+  content: '',
+});
+
+const variantClass = computed(() => {
+  switch (props.variant) {
+    case 'h1':
+      return 'text-h1 typography-h1';
+    case 'h2':
+      return 'text-h2 typography-h2';
+    case 'h3':
+      return 'text-h3 typography-h3';
+    case 'h4':
+      return 'text-h4 typography-h4';
+    case 'h5':
+      return 'text-h5 typography-h5';
+    case 'h6':
+      return 'text-h6 typography-h6';
+    case 'subtitle':
+      return 'text-subtitle1 typography-subtitle';
+    case 'body':
+      return 'text-body1 typography-body';
+    case 'caption':
+      return 'text-caption typography-caption';
+    case 'overline':
+      return 'text-overline typography-overline';
+    case 'blockquote':
+      return 'typography-blockquote';
+    default:
+      return '';
+  }
+});
+</script>
+
+<template>
+  <component :is="as" :class="[variantClass, $attrs.class]" v-bind="$attrs">
+    <template v-if="!$slots['default']">{{ content }}</template>
+    <slot />
+  </component>
+</template>
+
+<style scoped lang="scss">
+@import './_Typography.style.scss';
+</style>
