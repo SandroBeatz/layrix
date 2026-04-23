1023a3095510c651598a68dbf57a4ac1e0178301
Aleksandr M. <amikhailov@intelliants.com>
2026-04-23 10:55:42 +0600

    feat: update Velocorner widget integration to use new dashboard component

--- src/pages/velocorner-widget/VelocornerWidgetPage.vue
+++ src/pages/velocorner-widget/VelocornerWidgetPage.vue
@@ -7,7 +7,7 @@ let scriptElement: HTMLScriptElement | null = null;
 onMounted(() => {
   scriptElement = document.createElement('script');
   // scriptElement.src = 'https://velocorner-widget.vercel.app/velocorner-widget.iife.js';
-  scriptElement.src = 'https://velocorner-widget-dashboard.vercel.app/velocorner-dashboard.iife.js';
+  scriptElement.src = 'https://wgt.int.velocorner.ch/widget.iife.js';
   document.head.appendChild(scriptElement);
 });
 
@@ -23,8 +23,7 @@ onUnmounted(() => {
     title="Velocorner Widget V3"
     subtitle="An example page for Velocorner Widget V3 integration"
   >
-    <velocorner-dashboard secret-key="YOUR_BEARER_TOKEN" locale="en"></velocorner-dashboard>
-    <!--    <velocorner-widget api-key="111333"></velocorner-widget>-->
+    <velocorner-widget api-key="9cb20599-17c1-435c-82ee-4241d6f4b696"></velocorner-widget>
   </PageContainer>
 </template>
 
